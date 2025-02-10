72e6d4048a3160de33e250e34e31e0a9830c6c6d
Muhammad Saleh <143253075+shk-saleh@users.noreply.github.com>
2025-02-10 17:56:40 +0500

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
     <header className="border-b border-gray-800">
         <div className="max-w-8xl mx-auto px-4 py-4 sm:px-6 lg:px-8 flex justify-between items-center">
         <h1 className="text-2xl font-bold bg-gradient-to-r from-blue-400 to-violet-400 bg-clip-text text-transparent">
-            <NavLink to="/home">ResearchAI</NavLink>
+            <NavLink to="/">ResearchAI</NavLink>
         </h1>
         <div className="space-x-4">
             <button className="px-4 py-2 text-gray-300 hover:text-white transition-colors cursor-pointer"><NavLink to="/login">Login</NavLink></button>
